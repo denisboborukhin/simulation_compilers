b54a8df943a6f6b37741a5303b30690a6aaa8484
Denis <denis.boboruchin@gmail.com>
2022-11-05 23:49:13 +0300

    started executor, add first commands

--- RVinterpreter/interpreter/include/rv32_interpreter.hpp
+++ RVinterpreter/interpreter/include/rv32_interpreter.hpp
@@ -3,17 +3,21 @@
 #include <iostream>
 #include <vector>
 
-int interpret_rv32_bin_code (std::vector<int32_t> bin_code);
-
 class cpu final
 {
     static constexpr int NUM_REGS = 32;
-    int32_t regfile [NUM_REGS];
+    int32_t regfile [NUM_REGS] {};
     int pc = 0;
 
 public:
     int get_pc ();
+    int next_pc ();
     int set_pc (int val);
     int32_t get_reg (int num_reg);
     int32_t set_reg (int num_reg, int32_t val_reg);
 };
+
+void interpret_rv32_bin_code (std::string elf_file_name);
+int execute_instruction (cpu& cpu, std::pair<int64_t, std::vector<int32_t>>& bin_code);
+
+int get_bits (int32_t instruction, int left, int right);
--- RVinterpreter/interpreter/src/rv32_interpreter.cpp
+++ RVinterpreter/interpreter/src/rv32_interpreter.cpp
@@ -5,6 +5,11 @@ int cpu::get_pc ()
     return pc;
 }
 
+int cpu:: next_pc ()
+{
+    return ++pc;
+}
+
 int cpu::set_pc (int val)
 {
     pc = val;
@@ -28,10 +33,67 @@ int32_t cpu::set_reg (int num_reg, int32_t val_reg)
         exit (0);
 }
 
-int interpret_rv32_bin_code (std::vector<int32_t> bin_code)
+void interpret_rv32_bin_code (std::string elf_file_name)
 {
     cpu cpu;
+    std::pair<int64_t, std::vector<int32_t>> bin_code = get_bin_code (elf_file_name);       
 
+    while (true)
+    {
+        if (!execute_instruction (cpu, bin_code))
+            break;
+    }
+}
+
+int execute_instruction (cpu& cpu, std::pair<int64_t, std::vector<int32_t>>& bin_code)
+{
+    int pc = cpu.get_pc ();
+    if (pc >= bin_code.second.size ())
+        return 0;
+
+    int32_t instruction = bin_code.second[pc];
+   
+    switch (instruction & 0x7F)
+    {
+        case 0b0110011:
+        {
+            std::cout << "add!!!\n";
+            int rd = get_bits (instruction, 7, 11);
+			int rs1 = get_bits (instruction, 15, 19);
+			int imm = get_bits (instruction, 20, 24);      
+            
+            break;
+        }
+
+        case 0b0010011:
+        {
+            std::cout << "addi!!!";
+            int rd = get_bits (instruction, 7, 11);
+			int rs1 = get_bits (instruction, 15, 19);
+			int imm = get_bits (instruction, 20, 31);
+
+            std::cout << "rd: " << rd << std::endl;
+            std::cout << "imm: " << imm << std::endl;
+
+            auto funct3 = get_bits (instruction, 12, 14);
+
+            break;
+        }
+
+        default:
+        {
+            std::cout << bin_code.second.size () << std::endl;
+            break;
+        }
+    }
+    
+    cpu.next_pc ();
+    return 1;
+}
+
+int get_bits (int32_t instruction, int young, int old)
+{
+    return static_cast<int>(((instruction << (31 - old)) >> (31 - old)) >> young);
 }
 
 int main(int argc, char **argv)
@@ -41,9 +103,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    std::pair<int64_t, std::vector<int32_t>> bin_code = get_bin_code (argv[1]);        
-    int64_t address = bin_code.first;
-    std::vector<int32_t> instructions = bin_code.second;
+    interpret_rv32_bin_code (argv[1]);        
 
     return 0;
 }
